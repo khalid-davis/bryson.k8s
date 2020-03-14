ea1592af75f4bdf019897f1f8c6d70409fa81430
bryson-davis <qinkai1994@qq.com>
2020-03-14 22:06:40 +0800

    update

--- kubernetes-tester/controllers/rbac_validate.go
+++ kubernetes-tester/controllers/rbac_validate.go
@@ -14,15 +14,17 @@ var k8sClient k8s.Client
 
 var selfNamespace string
 var otherNamespace string
+var kubeconfigPath string
 
 func init() {
 	selfNamespace = "k8s-tester"
 	otherNamespace = "greenstock"
+	kubeconfigPath = "../kubeconfig"
 }
 
 // @router /inclusterselfnamespace [get]
 func (controller *RBACController) RBACInClusterSelfNamespace() {
-	k8sClient, err := k8s.NewK8sClient()
+	k8sClient, err := k8s.NewNativeK8sClientConfigByPath(kubeconfigPath)
 	if err != nil {
 		log.Println("err: ", err)
 		controller.Ctx.Output.SetStatus(400)
@@ -47,7 +49,7 @@ func (controller *RBACController) RBACInClusterSelfNamespace() {
 
 // @route /inclusterothernamespace [get]
 func (controller *RBACController) RBACInClusterOtherNamespace() {
-	k8sClient, err := k8s.NewK8sClient()
+	k8sClient, err := k8s.NewNativeK8sClientConfigByPath(kubeconfigPath)
 	if err != nil {
 		log.Println("err: ", err)
 		controller.Ctx.Output.SetStatus(400)
@@ -66,6 +68,32 @@ func (controller *RBACController) RBACInClusterOtherNamespace() {
 	log.Println("pod size: ", len(podList.Items))
 	controller.Ctx.Output.SetStatus(200)
 	controller.Data["json"] = podList.Items
+	controller.ServeJSON()
+	return
+}
+
+
+// @route /inclusterNode [get]
+func (controller *RBACController) RBACInClusterNode() {
+	k8sClient, err := k8s.NewNativeK8sClientConfigByPath(kubeconfigPath)
+	if err != nil {
+		log.Println("err: ", err)
+		controller.Ctx.Output.SetStatus(400)
+		controller.Data["json"] = err.Error()
+		controller.ServeJSON()
+		return
+	}
+	nodeList, err := k8sClient.Nodes()
+	if err != nil {
+		log.Println("err: ", err)
+		controller.Ctx.Output.SetStatus(400)
+		controller.Data["json"] = err.Error()
+		controller.ServeJSON()
+		return
+	}
+	log.Println("Node size: ", len(nodeList.Items))
+	controller.Ctx.Output.SetStatus(200)
+	controller.Data["json"] = nodeList.Items
 	controller.ServeJSON()
 	return
 }
--- kubernetes-tester/k8s/client.go
+++ kubernetes-tester/k8s/client.go
@@ -5,11 +5,13 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"log"
 )
 
 type Client interface {
 	Pods(namespace string) (*v1.PodList, error)
+	Nodes() (*v1.NodeList, error)
 }
 
 //默认客户端
@@ -19,7 +21,7 @@ type NativeK8sClient struct {
 	url string
 }
 
-func NewK8sClient() (Client, error) {
+func NewNativeK8sClient() (Client, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		log.Println("Error: ", err)
@@ -37,8 +39,27 @@ func NewK8sClient() (Client, error) {
 	}, nil
 }
 
+func NewNativeK8sClientConfigByPath(kubeconfigPath string) (Client, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return &NativeK8sClient{
+		Clientset: clientset,
+		config:    config,
+		url:       "",
+	}, nil
+}
+
 func (client *NativeK8sClient) Pods(namespace string) (*v1.PodList, error) {
 	return client.CoreV1().Pods(namespace).List(metav1.ListOptions{})
 }
 
+func (client *NativeK8sClient) Nodes() (*v1.NodeList, error) {
+	return client.CoreV1().Nodes().List(metav1.ListOptions{})
+}
 
